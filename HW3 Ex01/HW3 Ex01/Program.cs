4b556fef363a562f071a5c373166848778e6fd98
pedram <pedramalizade03@gmail.com>
2024-10-24 10:51:49 +0330

    six commit

---
+++ HW3 Ex01/HW3 Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter Your Number: ");
+string binaryInput = Console.ReadLine();
+
+bool validBinary = true;
+foreach (char c  in binaryInput)
+{
+    if (c != '0' && c != '1')
+    {
+        validBinary = false;
+        break;
+    }
+}
+if (validBinary)
+{
+    int decimalResult = Convert.ToInt32(binaryInput, 2);
+    Console.WriteLine($"decimal : {decimalResult}");
+}
+else
+{
+    Console.WriteLine("invalid input");
+}
---
+++ HW3 Ex02/HW3 Ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Please Enter Your Number: ");
+string input = Console.ReadLine();
+
+string[] inputNumbers = input.Split(',');
+int[] numbers = new int[inputNumbers.Length];
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    numbers[i] = int.Parse(inputNumbers[i]);
+}
+Array.Sort(numbers);
+int count = 1;
+string result = "";
+for (int i = 1; i <= numbers.Length; i++)
+{
+    if (i < numbers.Length && numbers[i] == numbers[i - 1])
+    {
+        count++;
+    }
+    else
+    {
+        if (count > 1)
+        {
+            result += $"{numbers[i - 1]}^{count}";
+        }
+        else
+        {
+            result += $"{numbers[i - 1]}";
+        }
+        if (i < numbers.Length)
+        {
+            result += "*";
+        }
+        count = 1;
+    }
+}
+Console.WriteLine(result);
